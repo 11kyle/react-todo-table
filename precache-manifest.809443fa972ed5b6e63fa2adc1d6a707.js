a0c8447584b9e8d04bedf5a3bf607b718282b300
Kyle <kyle@Kyles-MacBook-Pro.local>
2019-01-21 09:57:31 -0600

    Updates

---
+++ precache-manifest.809443fa972ed5b6e63fa2adc1d6a707.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "020e96b7cd4d8c479a39",
+    "url": "/react-todo-table/static/js/runtime~main.020e96b7.js"
+  },
+  {
+    "revision": "fd9501fd3b9b27f01ca7",
+    "url": "/react-todo-table/static/js/main.fd9501fd.chunk.js"
+  },
+  {
+    "revision": "f4789f78bc5099d1ff6c",
+    "url": "/react-todo-table/static/js/1.f4789f78.chunk.js"
+  },
+  {
+    "revision": "fd9501fd3b9b27f01ca7",
+    "url": "/react-todo-table/static/css/main.6daf1881.chunk.css"
+  },
+  {
+    "revision": "49ea4f553de159e5ea306123538cebfa",
+    "url": "/react-todo-table/index.html"
+  }
+];
